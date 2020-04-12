7f7f2cf6dfd5bd3ef0c61e6b5a0d9a75947bb279
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2020-04-12 17:47:05 +0700

    Search book by title & isbn

--- app/javascript/packs/users/layouts/header.js
+++ app/javascript/packs/users/layouts/header.js
@@ -1,17 +1,40 @@
-$(document).on('turbolinks:load', function() {
-  $('.search-toggle').click(function() {
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-      $('.search-form').removeClass('open');
+$('.search-toggle').click(function() {
+  if($(this).hasClass('active')){
+    $(this).removeClass('active');
+    $('.search-form').removeClass('open');
 
-      setTimeout (function(){
-        $('.navbar-nav').removeClass('hide');
-      }, 300);
-    }
-    else{
-      $(this).addClass('active');
-      $('.search-form').addClass('open');
-      $('.navbar-nav').addClass('hide');
-    }
-  });
+    setTimeout (function(){
+      $('.navbar-nav').removeClass('hide');
+    }, 300);
+  }
+  else{
+    $(this).addClass('active');
+    $('.search-form').addClass('open');
+    $('.navbar-nav').addClass('hide');
+  }
 });
+
+var timer;
+
+$('#search').keyup(function() {
+  $('#autocomplete').html($('#searching').html());
+  clearTimeout(timer);
+  var ms = 500;
+  var val = this.value;
+  timer = setTimeout(function() {
+    search(val);
+  }, ms);
+});
+
+function search(query) {
+  if(query.length > 0){
+    var url = '/search.json?query=' + query;
+    $.get(url, function(data, status) {
+      if(status === 'success'){
+        $('#autocomplete').html($(data.html_data));
+      }
+    });
+  } else {
+    $('#autocomplete').html('');
+  }
+}
