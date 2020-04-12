287f2622e91de1a1c415c0c960f65908a7124f96
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2020-04-12 14:43:01 +0700

    Admin layout

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -3,10 +3,9 @@ window.jQuery = $;
 window.$ = $;
 import 'jquery/dist/jquery.min';
 import 'bootstrap/dist/js/bootstrap.min';
-import '../stylesheets/users/custom';
+import '../stylesheets/admin/custom';
 import 'propellerkit/dist/js/propeller.min.js';
 require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-require('packs/users/layouts/header')
