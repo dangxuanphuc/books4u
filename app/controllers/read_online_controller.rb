1bc5534966d16f9e91b4afd94639c5ef5d5b8d16
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2020-04-21 20:29:14 +0700

    Read online

---
+++ app/controllers/read_online_controller.rb
@@ -0,0 +1,14 @@
+class ReadOnlineController < ApplicationController
+  before_action :find_ebooks
+
+  def show; end
+
+  private
+
+  def find_ebooks
+    @ebooks = Book.find_by(id: params[:id]).pdf_ebooks
+
+    return @ebooks
+    flash[:danger] = t "read_online.flash.danger.not_found_ebooks"
+  end
+end
