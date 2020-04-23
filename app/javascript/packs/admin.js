070a8e77d29145d137c37c8d5931316cd7023234
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2020-04-23 15:53:38 +0700

    User request

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
-import 'jquery/dist/jquery.min';
 import 'bootstrap/dist/js/bootstrap.min';
 import '../stylesheets/admin/custom';
 import 'propellerkit/dist/js/propeller.min.js';
