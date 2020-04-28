b64db084144f9661d8eda631bc07efd66947c96d
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2020-04-28 22:41:48 +0700

    Fix profile

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
     resource :rate, only: :create
   end
   resources :search, only: :index
+  resources :users, only: :show
   resources :categories, only: %i(index show)
   resources :borrows, only: :create
   resources :explorer, only: :index
